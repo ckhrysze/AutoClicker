6f92d266c8db36c8e46046c445b06cbe8462bc54
ChrisH <childebrand@shfl.com>
2014-12-04 15:59:05 -0800

    Adding everything again

---
+++ AutoClicker/Settings.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace AutoClicker
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstate = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.xLowerInput = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.xUpperInput = new System.Windows.Forms.TextBox();
+            this.yUpperInput = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.yLowerInput = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.num4 = new System.Windows.Forms.Label();
+            this.num3 = new System.Windows.Forms.Label();
+            this.num8 = new System.Windows.Forms.Label();
+            this.num7 = new System.Windows.Forms.Label();
+            this.num6 = new System.Windows.Forms.Label();
+            this.num5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.levelX = new System.Windows.Forms.Label();
+            this.activeWindowTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "23";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time Between Clicks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "milisec";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(9, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(259, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Press F1 to Start/Stop AutoClicking";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "AutoClicker State";
+            // 
+            // lstate
+            // 
+            this.lstate.AutoSize = true;
+            this.lstate.ForeColor = System.Drawing.Color.Red;
+            this.lstate.Location = new System.Drawing.Point(148, 58);
+            this.lstate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lstate.Name = "lstate";
+            this.lstate.Size = new System.Drawing.Size(64, 13);
+            this.lstate.TabIndex = 7;
+            this.lstate.Text = "Not Clicking";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "X Bounds";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Y Bounds";
+            // 
+            // xLowerInput
+            // 
+            this.xLowerInput.Location = new System.Drawing.Point(70, 97);
+            this.xLowerInput.Name = "xLowerInput";
+            this.xLowerInput.Size = new System.Drawing.Size(46, 20);
+            this.xLowerInput.TabIndex = 10;
+            this.xLowerInput.Text = "1050";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(122, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "to";
+            // 
+            // xUpperInput
+            // 
+            this.xUpperInput.Location = new System.Drawing.Point(144, 97);
+            this.xUpperInput.Name = "xUpperInput";
+            this.xUpperInput.Size = new System.Drawing.Size(46, 20);
+            this.xUpperInput.TabIndex = 12;
+            this.xUpperInput.Text = "1450";
+            // 
+            // yUpperInput
+            // 
+            this.yUpperInput.Location = new System.Drawing.Point(144, 118);
+            this.yUpperInput.Name = "yUpperInput";
+            this.yUpperInput.Size = new System.Drawing.Size(46, 20);
+            this.yUpperInput.TabIndex = 15;
+            this.yUpperInput.Text = "700";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(122, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "to";
+            // 
+            // yLowerInput
+            // 
+            this.yLowerInput.Location = new System.Drawing.Point(70, 118);
+            this.yLowerInput.Name = "yLowerInput";
+            this.yLowerInput.Size = new System.Drawing.Size(46, 20);
+            this.yLowerInput.TabIndex = 13;
+            this.yLowerInput.Text = "300";
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Location = new System.Drawing.Point(243, 61);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(25, 13);
+            this.num1.TabIndex = 16;
+            this.num1.Text = "1 ->";
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Location = new System.Drawing.Point(243, 113);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(25, 13);
+            this.num2.TabIndex = 17;
+            this.num2.Text = "2 ->";
+            // 
+            // num4
+            // 
+            this.num4.AutoSize = true;
+            this.num4.Location = new System.Drawing.Point(243, 217);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(25, 13);
+            this.num4.TabIndex = 19;
+            this.num4.Text = "4 ->";
+            // 
+            // num3
+            // 
+            this.num3.AutoSize = true;
+            this.num3.Location = new System.Drawing.Point(243, 165);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(25, 13);
+            this.num3.TabIndex = 18;
+            this.num3.Text = "3 ->";
+            // 
+            // num8
+            // 
+            this.num8.AutoSize = true;
+            this.num8.Location = new System.Drawing.Point(243, 425);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(25, 13);
+            this.num8.TabIndex = 23;
+            this.num8.Text = "8 ->";
+            // 
+            // num7
+            // 
+            this.num7.AutoSize = true;
+            this.num7.Location = new System.Drawing.Point(243, 373);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(25, 13);
+            this.num7.TabIndex = 22;
+            this.num7.Text = "7 ->";
+            // 
+            // num6
+            // 
+            this.num6.AutoSize = true;
+            this.num6.Location = new System.Drawing.Point(243, 321);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(25, 13);
+            this.num6.TabIndex = 21;
+            this.num6.Text = "6 ->";
+            // 
+            // num5
+            // 
+            this.num5.AutoSize = true;
+            this.num5.Location = new System.Drawing.Point(243, 269);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(25, 13);
+            this.num5.TabIndex = 20;
+            this.num5.Text = "5 ->";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Level   x";
+            // 
+            // levelX
+            // 
+            this.levelX.AutoSize = true;
+            this.levelX.Location = new System.Drawing.Point(55, 165);
+            this.levelX.Name = "levelX";
+            this.levelX.Size = new System.Drawing.Size(13, 13);
+            this.levelX.TabIndex = 25;
+            this.levelX.Text = "1";
+            // 
+            // activeWindowTitle
+            // 
+            this.activeWindowTitle.AutoSize = true;
+            this.activeWindowTitle.Location = new System.Drawing.Point(13, 216);
+            this.activeWindowTitle.Name = "activeWindowTitle";
+            this.activeWindowTitle.Size = new System.Drawing.Size(41, 13);
+            this.activeWindowTitle.TabIndex = 26;
+            this.activeWindowTitle.Text = "label10";
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 441);
+            this.Controls.Add(this.activeWindowTitle);
+            this.Controls.Add(this.levelX);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.yUpperInput);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.yLowerInput);
+            this.Controls.Add(this.xUpperInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.xLowerInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Settings";
+            this.Text = "AutoClicker";
+            this.Load += new System.EventHandler(this.SettingsFormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lstate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox xLowerInput;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox xUpperInput;
+        private System.Windows.Forms.TextBox yUpperInput;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox yLowerInput;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.Label num4;
+        private System.Windows.Forms.Label num3;
+        private System.Windows.Forms.Label num8;
+        private System.Windows.Forms.Label num7;
+        private System.Windows.Forms.Label num6;
+        private System.Windows.Forms.Label num5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label levelX;
+        private System.Windows.Forms.Label activeWindowTitle;
+    }
+}
+
---
+++ AutoClicker/Settings.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing;
+
+namespace AutoClicker
+{
+    public partial class Settings : Form
+    {
+        static Thread AutoClick;
+        static Thread InfoScrapper;
+
+        static int militime;
+        static int xLowerBound;
+        static int xUpperBound;
+        static int yLowerBound;
+        static int yUpperBound;
+        static int yCoord;
+        public static int WM_HOTKEY = 0x312;
+
+
+        public void AClick()
+        {
+            while (true)
+            {
+                int x = Cursor.Position.X;
+                int y = Cursor.Position.Y;
+                if (xLowerBound < x && x < xUpperBound && yLowerBound < y && y < yUpperBound)
+                {
+                    clickAt(Cursor.Position);
+                }
+                Thread.Sleep(militime);
+            }
+        }
+
+        public void Scrapper()
+        {
+            while(true)
+            {
+                var title = User32DllWrapper.GetActiveWindowTitle();
+                activeWindowTitle.Invoke((MethodInvoker)(() =>
+                    activeWindowTitle.Text = title
+                    ));
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void clickAt(Point p)
+        {
+            User32DllWrapper.click(p);
+        }
+
+        public Settings()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+        private void SettingsFormLoad(object sender, EventArgs e)
+        {
+            User32DllWrapper.RegisterHotKey(this.Handle, (int)Keys.F1, 0, (uint)Keys.F1);
+
+            setupHotKey(Keys.NumPad1);
+            setupHotKey(Keys.NumPad2);
+            setupHotKey(Keys.NumPad3);
+            setupHotKey(Keys.NumPad4);
+            setupHotKey(Keys.NumPad5);
+            setupHotKey(Keys.NumPad6);
+            setupHotKey(Keys.NumPad7);
+            setupHotKey(Keys.NumPad8);
+            setupHotKey(Keys.NumPad9);
+
+            setupHotKey(Keys.Add);
+            setupHotKey(Keys.Subtract);
+
+            AutoClick = new Thread(AClick);
+            AutoClick.IsBackground = true;
+
+            InfoScrapper = new Thread(Scrapper);
+            InfoScrapper.IsBackground = true;
+            InfoScrapper.Start();
+        }
+
+        private void setupHotKey(Keys key) { User32DllWrapper.RegisterHotKey(this.Handle, (int)key, 0, (uint)key); }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+
+                switch (key)
+                {
+                    case Keys.F1: this.f1Handler(); break;
+                    case Keys.NumPad1: this.levelHero(this.num1); break;
+                    case Keys.NumPad2: this.levelHero(this.num2); break;
+                    case Keys.NumPad3: this.levelHero(this.num3); break;
+                    case Keys.NumPad4: this.levelHero(this.num4); break;
+                    case Keys.NumPad5: this.levelHero(this.num5); break;
+                    case Keys.NumPad6: this.levelHero(this.num6); break;
+                    case Keys.NumPad7: this.levelHero(this.num7); break;
+                    case Keys.NumPad8: this.levelHero(this.num8); break;
+                    case Keys.NumPad9: this.allUpgrades(); break;
+                    case Keys.Add: this.increaseLevelMultiplier(); break;
+                    case Keys.Subtract: this.decreaseLevelMultiplier(); break;
+
+                }
+            }
+        }
+
+        private void decreaseLevelMultiplier()
+        {
+            int multiplier = int.Parse(levelX.Text);
+            switch (multiplier)
+            {
+                case 100: multiplier = 25; break;
+                case 25: multiplier = 10; break;
+                case 10: multiplier = 1; break;
+            }
+            levelX.Text = multiplier.ToString();
+        }
+
+        private void increaseLevelMultiplier()
+        {
+            int multiplier = int.Parse(levelX.Text);
+            switch (multiplier)
+            {
+                case 1: multiplier = 10; break;
+                case 10: multiplier = 25; break;
+                case 25: multiplier = 100; break;
+            }
+            levelX.Text = multiplier.ToString();
+        }
+
+        private void allUpgrades()
+        {
+            Label l = num7;
+            var offset = 300;
+            Point absPoint = this.PointToScreen(l.Location);
+            Point toTheRight = new Point(absPoint.X + offset, absPoint.Y);
+            User32DllWrapper.clickAndReturn(toTheRight, Cursor.Position);
+            
+        }
+
+        private void levelHero(Label l)
+        {
+            checkMultiplier();
+            var offset = 100;
+            Point absPoint = this.PointToScreen(l.Location);
+            Point toTheRight = new Point(absPoint.X + offset, absPoint.Y);
+            User32DllWrapper.clickAndReturn(toTheRight, Cursor.Position);
+            releaseMultiplier();
+        }
+
+        private void releaseMultiplier()
+        {
+            switch (int.Parse(levelX.Text))
+            {
+                case 10: User32DllWrapper.SendKeyUp((ushort)Keys.LShiftKey); break;
+                case 25: User32DllWrapper.SendKeyUp((ushort)Keys.Z); break;
+                case 100: User32DllWrapper.SendKeyUp((ushort)Keys.ControlKey); break;
+            }
+            Thread.Sleep(25);
+        }
+
+        private void checkMultiplier()
+        {
+            switch (int.Parse(levelX.Text))
+            {
+                case 10: User32DllWrapper.SendKeyDown((ushort)Keys.LShiftKey); break;
+                case 25: User32DllWrapper.SendKeyDown((ushort)Keys.Z); break;
+                case 100: User32DllWrapper.SendKeyDown((ushort)Keys.ControlKey); break;
+            }
+        }
+
+        private void f1Handler()
+        {
+            if (!AutoClick.IsAlive)
+            {
+                try { captureInputs(); }
+                catch { setDefaults(); }
+
+                AutoClick.Start();
+                lstate.Text = "Clicking";
+                lstate.ForeColor = System.Drawing.Color.Green;
+                Controls.Add(lstate);
+            }
+            else
+            {
+                AutoClick.Abort();
+                AutoClick = new Thread(AClick);
+                lstate.Text = "Not Clicking";
+                lstate.ForeColor = System.Drawing.Color.Red;
+                Controls.Add(lstate);
+            }
+        }
+
+        private void captureInputs()
+        {
+            militime = int.Parse(textBox1.Text);
+            xLowerBound = int.Parse(xLowerInput.Text);
+            xUpperBound = int.Parse(xUpperInput.Text);
+            yLowerBound = int.Parse(yLowerInput.Text);
+            yUpperBound = int.Parse(yUpperInput.Text);
+        }
+
+        private void setDefaults()
+        {
+            militime = 23;
+            xLowerBound = 1200;
+            xUpperBound = 1300;
+            yLowerBound = 500;
+            yUpperBound = 600;
+        }
+
+    }
+}
